60d49fe2740d3e2fb2ef3dd005449d173cd85f5b
asre459 <asre45900@gmail.com>
2024-03-01 15:36:46 -0300

    my first java programm

---
+++ asre.java
@@ -0,0 +1,201 @@
+// import java.util.Scanner;
+
+// public class asre {
+//     public static void main(String[] args) {
+//     Scanner  scanner = new Scanner(System.in);
+// int rows;
+// int columns;
+// String symbol = "";
+// System.out.println("Enter # of rows: ");
+// rows = scanner.nextInt();
+// System.out.println("Enter # of columns: ");
+// columns = scanner.nextInt();
+// System.out.println("Enter symbol to use: ");
+// symbol = scanner.next(); 
+// for(int i = 1; i <= rows ; i++)
+// System.out.println();
+//     }
+    
+// }
+// public class asre {
+     
+//     public static void main(String[] args) {
+//       System.out.println("The two numbers is");
+//       int x = 45;
+//       int y = 34;
+//       System.out.println(x +" " +y);
+//       int sum = x+y;
+//       int mult= x*y;
+//       System.out.println("the sum is"+"" +sum);
+//       System.out.println("The multiplication is :" +mult);
+      
+// //     }
+// //    }
+// public class asre {
+
+//     public static void main(String[] args) {
+//         int a=3;
+//         int b= 5;
+//         int c;
+//         System.out.println("swap the number:");
+//         System.out.println(" a is \t" +a);
+//         System.out.println("b is \t" +b);
+//         c=a;
+//         a= b;
+//         b=c;
+//         System.out.println("the value of a and b are "  );
+//         System.out.println( a);
+//         System.out.println(b);
+//     }
+// }
+
+// import java.util.Scanner;
+
+// /**
+//  * asre
+//  */
+// public class asre {
+
+//     public static void main(String[] args) {
+//         Scanner a =new Scanner(System.in);
+//     System.out.println("Enter the double number");
+//     double y= a.nextDouble();
+//     System.out.println("you are entered " + y);
+//     a.close();
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class asre {
+
+//     public static void main(String[] args) {
+//         Scanner name = new Scanner(System.in);
+//         System.out.println("enter the name of student:");
+//         String student= name.nextLine();
+//         System.out.println(" the name is " + student+ ".");
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class asre {
+
+//     public static void main(String[] args){
+//         Scanner x = new Scanner(System.in);
+//         Scanner y = new Scanner(System.in);
+//         Scanner z = new Scanner(System.in);
+//         System.out.println("Enter the First number:");
+//         System.out.println("Enter the  second number:");
+//         System.out.println("Enter the thrid number:");
+//         int a = x.nextInt();
+//         int b = y.nextInt();
+//         int c = z.nextInt();
+//         int sum =(a + b +c)/3;
+//         System.out.println(sum);
+
+
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+
+// public class asre {
+
+//     public static void main(String[] args) {
+//         Scanner x =  new Scanner(System.in);
+//         System.out.println("Enter the number from key board");
+//         int y = x.nextInt();
+//         System.out.println("the number is :" + y);
+
+//     }
+// }
+// public  class asre {
+
+//     public void show(){
+//         int num =109;//local variable
+//         System.out.println("The number is :" +num);
+//     }
+//     public static void main(String[] args) {
+//         asre test=new asre();
+//         test.show();
+//     }
+//
+// public  class asre {
+//     //statics variable
+//     int num;
+//     static int count = 0;
+//     asre( int n){
+//         num= n;
+//         count++;
+//     }
+//         public void show(){
+           
+//             System.out.println("The number is :" +num);
+//         }
+//         public static void main(String[] args) {
+//             asre test=new asre(97);
+//             test.show(); 
+//             asre test1=new asre(98);
+//             test1.show();
+//             System.out.println( "The total object:" +count);
+//         }
+//     }
+/**
+ * asre
+//  */
+// public class asre {
+
+//     public static void main(String[] args) {
+//         boolean var2 = true;
+//         if ( var2== true) {
+//             System.out.println("it is true");
+//         }
+//         else{
+//             System.out.println("it is false");
+//         }
+//     }
+// }
+/**
+ * asre
+ */
+// public class asre {
+// public static void main(String[] args) {
+//     int a= 30;
+//     short s = 4;
+//     byte  h =66;
+//     long t= 645783595;
+//     float f= 76;
+//     double d= 465.67;
+//     int y =(int)d;
+//     System.out.println(a);
+//     System.out.println(s);
+//     System.out.println(h);
+//     System.out.println(f);
+//     System.out.println();
+//     System.out.println(y);
+
+//     System.out.println(t);
+
+// }
+// }
+/**
+ * asre this is unary operator 
+//  */
+/**
+ * asre
+ */
+public class asre {
+
+    public static void main(String[] args) {
+        int a= 12;
+        int b= 5;
+        int c= 30;
+        System.out.println( a<b||a<c);
+        System.out.println(a<b&&a<c);
+        System.out.println();
+        System.out.println();
+    }
+}
